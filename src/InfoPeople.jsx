5159e22ea7f02e7c03b446504566ba3b689c8d8d
nurem1r <145115416+nurem1r@users.noreply.github.com>
2024-11-20 18:31:35 +0600

    Adding button and information

---
+++ src/InfoPeople.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function InfoPeople() {
+  return (
+    <div>
+      <h1>Информация о человеке</h1>
+      <p>Имя: Сулайманов Нурэмир</p>
+      <p>Должность: Разработчик</p>
+      <p>Компания: Sunrise Osh</p>
+      <p>Возраст: 20</p>
+      <p>Город: Бишкек</p>
+      <p>Хобби: Учиться</p>
+      <button>Кнопка</button>
+    </div>
+  );
+}
+
+export default InfoPeople;
