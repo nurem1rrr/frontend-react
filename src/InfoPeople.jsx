c52e23c10e15a7c46c1710424459b83ecb899163
nurem1r <145115416+nurem1r@users.noreply.github.com>
2024-11-20 19:08:07 +0600

    function of button

--- src/InfoPeople.jsx
+++ src/InfoPeople.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 
 function InfoPeople() {
-  const [message, setMessage] = useState("");
+  const [position, setPosition] = useState("Разработчик");
 
-  const handleClick = () => {
-    setMessage("над кнопкой");
+  const handlePromotion = () => {
+    setPosition("Старший разработчик");
   };
 
   return (
     <div>
       <h1>Информация о человеке</h1>
       <p>Имя: Сулайманов Нурэмир</p>
-      <p>Должность: Разработчик</p>
+      <p>Должность: {position}</p>
       <p>Компания: Sunrise Osh</p>
       <p>Возраст: 20</p>
       <p>Город: Бишкек</p>
       <p>Хобби: Учиться</p>
-      {message && <p>{message}</p>}
-      <button onClick={handleClick}>Повысить в должности</button>
+      <button onClick={handlePromotion}>Повысить в должности</button>
     </div>
   );
 }
 
 export default InfoPeople;
-
